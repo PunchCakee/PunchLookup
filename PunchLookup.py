d5f0ef31e5581d324b88dacc9bbe463cd4cb0eba
OgPunchCake <80383169+OgPunchCake@users.noreply.github.com>
2022-01-26 12:23:09 +0300

    Update PunchLookup.py

--- PunchLookup.py
+++ PunchLookup.py
@@ -1,12 +1,15 @@
 import random
 import requests
 import sys
-inp = input("what ip do you want to lookup?")
+import time 
+inp = input("what ip do you want to lookup? ")
 response = requests.get("http://ip-api.com/json/"+ inp).json()
 f = open("output.txt" , "w")
-print("country = " + response['country'] , file=f)
-print("city = " + response['city'] ,file=f)
-print("the isp of this ip is = " + response['isp'] ,file=f)
-print("latitude = "+ str(response['lat']) ,file=f)
-print("longtuide = " + str(response['lon']) ,file=f)
-print("details save in output.txt file, Enjoy :)")
+f.write("country = " + response['country'])
+f.write("\ncity = " + response['city'])
+f.write("\nthe isp of this ip is = " + response['isp'])
+f.write("\nlatitude = "+ str(response['lat']) )
+f.write("\nlongtuide = " + str(response['lon']) )
+print("output has been saved in output.txt")
+f.close()
+time.sleep(5)
