9db773fa3003c1977dd5391f582192fcc525d595
PunchCakee <80383169+PunchCakee@users.noreply.github.com>
2022-02-04 15:05:44 +0300

    Update PunchLookup.py

--- PunchLookup.py
+++ PunchLookup.py
@@ -1,15 +1,38 @@
 import random
-import requests
 import sys
-import time 
-inp = input("what ip do you want to lookup? ")
-response = requests.get("http://ip-api.com/json/"+ inp).json()
+import time
+import asyncio
+import httpx
+from io import TextIOWrapper
+from colorama import Fore
+inp = open('ips.txt', 'r')
 f = open("output.txt" , "w")
-f.write("country = " + response['country'])
-f.write("\ncity = " + response['city'])
-f.write("\nthe isp of this ip is = " + response['isp'])
-f.write("\nlatitude = "+ str(response['lat']) )
-f.write("\nlongtuide = " + str(response['lon']) )
-print("output has been saved in output.txt")
+async def main():
+    for line in inp:
+        async with httpx.AsyncClient(follow_redirects=False, timeout=1000) as client:
+            url = "http://ip-api.com/json/" + str(line.rstrip())
+            r = await client.get(url)
+            if r.status_code == 200:
+                data = r.json()
+                country = data.get("country", "Not Found")
+                city = data.get("city", "Not Found")
+                isp = data.get("isp", "Not Found")
+                lat = data.get("lat", "Not Found")
+                long = data.get("lon", "Not Found")
+                ip = data.get("query", "Not Found")
+                print(Fore.GREEN + "REQUEST SUCCEEDED!")
+                f.write("\nip: " + str(ip))
+                f.write("\ncountry : " + country)
+                f.write("\ncity : " + city)
+                f.write("\nisp : " + isp)
+                f.write("\nlatitude : " + str(lat))
+                f.write("\nlongitude : " + str(long))
+                f.write("\n")
+            else:
+                print(Fore.RED + "REQUEST FAILED")
+            if r.status_code == 429:
+                print(Fore.YELLOW + "Request limit exceeded, Sleeping for 1 min")
+                await asyncio.sleep(60)
+asyncio.run(main())
 f.close()
-time.sleep(5)
+inp.close()
